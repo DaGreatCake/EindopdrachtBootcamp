ba6dda25682414d096aac45b3a81f9fb2ba96344
Benjamin Veldhuizen <benjaminveld02@gmail.com>
2022-04-21 10:21:36 +0200

    Initial commit

---
+++ src/test/java/nl/bd/eindopdrachtbootcamp/EindopdrachtBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bd.eindopdrachtbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EindopdrachtBootcampApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
